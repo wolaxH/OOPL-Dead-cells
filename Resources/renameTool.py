7e0685af662b4e7b43146070174bb3231c732066
Etho <leseshen@gmail.com>
2025-03-19 09:30:06 +0800

    sync to laptop

--- Resources/renameTool.py
+++ Resources/renameTool.py
@@ -1,15 +1,19 @@
 import os
 from colorama import Fore, Style
+from natsort import natsorted
 
 #Plaese check that your res are sorted, this Tool has no sort function
+"""
+    you have to install natsort. 
+    if you dont have, you can use terminal and type "pip install natsort" to install
+"""
 
+Path = os.getcwd().replace("\\", "/") + "/Zombie/move"   #The folder path that you will replac
 
-Path = os.getcwd().replace("\\", "/") + "/Beheaded/runA"   #The folder path that you will replac
-
-prefix = "runA_"    #The new name prefix eg: runB_{index}.png
+prefix = "move_"    #The new name prefix eg: runB_{index}.png
 errorItem = []
 
-for i, old_name in enumerate(os.listdir(Path), start = 0):
+for i, old_name in enumerate(natsorted(os.listdir(Path)), start = 0):
     old_path = os.path.join(Path, old_name)
     new_name = f"{prefix}{i}{os.path.splitext(old_name)[1]}"
     new_path = os.path.join(Path, new_name)
