e1b0f95addedf847e3e0d4534bfbd9a62ac0632e
black94idd <kevin87945@gmail.com>
2025-05-08 22:02:08 +0800

    Map Update
    * commit
    
    * commit
    
    * test
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: kevin <t112590018@ntut.org.tw>

--- src/App/AppInGameInit.cpp
+++ src/App/AppInGameInit.cpp
@@ -33,12 +33,13 @@ void App::InGameInit() {
 
     //bg
     glm::vec2 temp_pos(1, 1);
-    BGs.resize(4);
+    BGs.resize(6);
     BGs[0] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg1.png", temp_pos, glm::vec2{0.8f, 0.8f});
     BGs[1] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg2.png", glm::vec2{1616.0f, 174.0f}, glm::vec2{0.9f, 0.9f});
     BGs[2] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg3.png", glm::vec2{3817.0f, 318.5f}, glm::vec2{1.4f, 1.4f});
-    BGs[3] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg4.png", glm::vec2{1616.0f, -673.0f}, glm::vec2{0.9f, 0.9f});
-
+    BGs[3] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg5.png", glm::vec2{6309.5f, 316.5f}, glm::vec2{1.2f, 1.2f});
+    BGs[4] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg6.png", glm::vec2{6309.5f, -970.0f}, glm::vec2{1.2f, 1.2f});
+    BGs[5] = std::make_shared<BG>(RESOURCE_DIR"/bg/bg4.png", glm::vec2{1616.0f, -673.0f}, glm::vec2{0.9f, 0.9f});
     for (auto& temp : BGs){
         MapObjs.push_back(temp);
         root.AddChild(temp);
